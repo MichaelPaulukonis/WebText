959a5a6e2df171e8e758c175febb46e0407a06b5
Michael Paulukonis <xraysmalevich@gmail.com>
2013-10-14 00:02:12 -0400

    proof-of-concept for infinitite text-space (2-d draggable version)

--- javascript/libs/markov2.charng.js
+++ javascript/libs/markov2.charng.js
@@ -223,7 +223,7 @@ var markov = function(opts) {
             word = getNext();
         } else {
             // this is not a good model for non-markov words
-                // if it is returning one char, yeah
+            // if it is returning one char, yeah
             // but it is NOT returning one char at a time
             // so: ugh
             // whitespace can be in the middle of the return value
